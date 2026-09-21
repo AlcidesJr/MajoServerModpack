74c301e3a9ffbae8eef1e19800a4ddb360a0469a
AlcidesJr <51201087+AlcidesJr@users.noreply.github.com>
2026-09-21 14:37:03 -0300

    feat(MAJO-002): activate protocol version 1

--- src/MajoServerModpack/Core/Runtime/MajoVersions.cs
+++ src/MajoServerModpack/Core/Runtime/MajoVersions.cs
@@ -2,8 +2,8 @@ namespace MajoServerModpack.Core.Runtime
 {
     public static class MajoVersions
     {
-        public const string MajoVersion = "0.0.1";
-        public const int ProtocolVersion = 0;
+        public const string MajoVersion = "0.0.2";
+        public const int ProtocolVersion = 1;
         public const int ConfigSchema = 0;
         public const int DataSchema = 0;
     }
